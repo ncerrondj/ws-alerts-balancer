faf16fcaf5dc73fe81044bcbcf0a1ea8a058085a
Nick <nickcerron@gmail.com>
2026-03-21 12:02:21 -0500

    monitorie 1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
+  setInterval(() => {
+      const used = process.memoryUsage();
+      console.log('RAM MB:', used.heapUsed / 1024 / 1024);
+  }, 2000);
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors();
   await app.listen(process.env.PORT ?? 5001);
 }
